0049cb5207eb5bcdf49a353238e063a0636ed927
Valentina <loghin_valentina@yahoo.com>
2023-07-18 10:19:28 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-// import { useState } from 'react'
+import "./App.css";
 import { Header } from "./Components/Header";
 import { ArticlesList } from "./Components/ArticlesList";
-import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
-      <ArticlesList />
+      <Routes>
+        <Route exact path="/" element={<ArticlesList />} />
+      </Routes>
     </div>
   );
 }
