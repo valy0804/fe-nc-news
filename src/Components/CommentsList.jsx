85eb24fbf020cb05bc77e566707d54a808214381
Valentina <loghin_valentina@yahoo.com>
2023-07-18 15:56:36 +0300

    add getCommentsByArticleId func and loading state

--- src/Components/CommentsList.jsx
+++ src/Components/CommentsList.jsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
+import { getCommentsByArticleId } from "../Utils/api";
 import { CommentCard } from "./CommentCard";
 
-export const CommentsList = ({ comments }) => {
+export const CommentsList = ({ article_id }) => {
+  const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    getCommentsByArticleId(article_id)
+      .then((comments) => {
+        setComments(comments);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching comments:", error);
+        setLoading(false);
+      });
+  }, [article_id]);
+
+  if (loading) {
+    return <div>Loading comments...</div>;
+  }
+
   return (
     <div className="comments-list">
-      <h2 className="list-header"> Comments</h2>
+      <h2 className="list-header">Comments</h2>
       {comments.map((comment) => (
-        <div className="comment-card" key={comment.comment_id}>
-          <CommentCard key={comment.comment_id} comment={comment} />
-        </div>
+        <CommentCard key={comment.comment_id} comment={comment} />
       ))}
     </div>
   );
