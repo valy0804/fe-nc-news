0a8fafc1ece218450ee38e1e15a40e0dc776eff3
Valentina <loghin_valentina@yahoo.com>
2023-07-18 15:56:57 +0300

    remove getCommentsByArticleId func

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -1,34 +1,30 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getArticleById, getCommentsByArticleId } from "../Utils/api";
+import { getArticleById } from "../Utils/api";
 import { CommentsList } from "./CommentsList";
 
 export const SingleArticle = () => {
   const { article_id } = useParams();
 
   const [article, setArticle] = useState(null);
-  const [comments, setComments] = useState([]);
-  const [loading, setLoading] = useState(true);
+
+  const [loadingArticle, setLoadingArticle] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
+    setLoadingArticle(true);
 
     getArticleById(article_id)
       .then(({ article }) => {
         setArticle(article);
-        return getCommentsByArticleId(article_id);
-      })
-      .then((comments) => {
-        setComments(comments);
-        setLoading(false);
+        setLoadingArticle(false);
       })
       .catch((error) => {
         console.error("Error fetching article:", error);
-        setLoading(false);
+        setLoadingArticle(false);
       });
   }, [article_id]);
 
-  if (loading) {
+  if (loadingArticle) {
     return <div>Loading...</div>;
   }
 
@@ -42,7 +38,7 @@ export const SingleArticle = () => {
       <img src={article.article_img_url} alt={article.title} />
       <p>Topic: {article.topic}</p>
       <p>{article.body}</p>
-      <CommentsList comments={comments} />
+      <CommentsList article_id={article_id} />
     </div>
   );
 };
