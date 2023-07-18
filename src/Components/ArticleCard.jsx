837f72199d21777d982d434c02b465cd2faae8a9
Valentina <loghin_valentina@yahoo.com>
2023-07-18 10:21:04 +0300

    format the date

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,11 +1,12 @@
 export const ArticleCard = (article) => {
   const { title, article_img_url, created_at } = article.article;
+  const formattedDate = new Date(created_at).toLocaleString();
 
   return (
     <div>
       <h3>{title}</h3>
       <img src={article_img_url} />
-      <p>Date: {created_at}</p>
+      <p>Date: {formattedDate}</p>
     </div>
   );
 };
