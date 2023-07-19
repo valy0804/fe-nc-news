77a798c216359eea13cd6800f0e274bc43e2273e
Valentina <loghin_valentina@yahoo.com>
2023-07-19 14:04:36 +0300

    create func patchArticleById

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -29,3 +29,39 @@ export const getCommentsByArticleId = (article_id) => {
     return res.data.comments;
   });
 };
+
+// export const updateVotesOnServer = (article_id, votes) => {
+//   return api.patch(`/api/articles/${article_id}`, { votes: votes });
+// };
+
+// export const updateVotesOnServer = (article_id, votes) => {
+//   return new Promise((resolve, reject) => {
+//     api
+//       .put(`/api/articles/${article_id}`, { votes: votes })
+//       .then((response) => {
+//         // Assuming the response contains the updated article data or a success message
+//         resolve(response.data);
+//       })
+//       .catch((error) => {
+//         // Handle the error here
+//         console.error("Error updating votes on server:", error);
+//         reject(error);
+//       });
+//   });
+// };
+
+const ncNewsApi = axios.create({
+  baseURL: "https://nc-news-ddgf.onrender.com/api",
+});
+
+export const patchArticleById = (article_id, inc_votes) => {
+  const articlePatchBody = {
+    article_id,
+    inc_votes,
+  };
+  return ncNewsApi
+    .patch(`/articles/${article_id}`, articlePatchBody)
+    .then((res) => {
+      return res;
+    });
+};
