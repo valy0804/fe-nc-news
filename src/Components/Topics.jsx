ba37ad43a3d0c0f346f6e9f29d9baceb0d6ffd66
Valentina <loghin_valentina@yahoo.com>
2023-07-21 12:21:46 +0300

    add topics component

--- src/Components/Topics.jsx
+++ src/Components/Topics.jsx
@@ -2,36 +2,25 @@ import Button from "react-bootstrap/Button";
 import { useState, useEffect } from "react";
 import { getTopics } from "../Utils/api";
 import { Link } from "react-router-dom";
-import { ArticleByTopic } from "../Pages/ArticlesByTopic";
 
 export const Topics = () => {
   const [topics, setTopics] = useState([]);
 
   useEffect(() => {
-    // Fetch the topics when the component mounts using the getTopics function
     getTopics()
-      .then((topicsData) => {
-        // Update the component state with the fetched topics data
-        setTopics(topicsData);
+      .then((res) => {
+        setTopics(res);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [topics]);
+  }, []);
 
   return (
     <div>
       {topics.map((topic) => (
-        <Link
-          key={topic.slug}
-          to={`articles/${topic.slug}`}
-          element={<ArticleByTopic />}
-        >
-          <Button
-            variant="secondary"
-            key={topic.slug}
-            className="topics-button"
-          >
+        <Link key={topic.slug} to={`/topics/${topic.slug}`}>
+          <Button variant="secondary" className="topics-button">
             {topic.slug}
           </Button>
         </Link>
