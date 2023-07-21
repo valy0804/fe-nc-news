14fd46a4ffe23d12d89831c64fd5260754a01bc1
Valentina <loghin_valentina@yahoo.com>
2023-07-21 12:22:53 +0300

    add route for topics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Header } from "./Components/Header";
 import { ArticlesList } from "./Components/ArticlesList";
 import { SingleArticle } from "./Components/SingleArticle";
+import { ArticlesByTopic } from "./Pages/ArticlesByTopic";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<ArticlesList />} />
         <Route path="/articles/:article_id" element={<SingleArticle />} />
+        <Route path="/topics/:topicSlug" element={<ArticlesByTopic />} />
       </Routes>
     </div>
   );
