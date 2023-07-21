29fa0667d77261b1d14b523cbd08ff4040d07965
Valentina <loghin_valentina@yahoo.com>
2023-07-21 12:22:11 +0300

    add articles by topic component

--- src/Pages/ArticlesByTopic.jsx
+++ src/Pages/ArticlesByTopic.jsx
@@ -3,22 +3,36 @@ import { getArticlesByTopic } from "../Utils/api";
 import { ArticleCard } from "../Components/ArticleCard";
 import { Link, useParams } from "react-router-dom";
 
-export const ArticleByTopic = () => {
+export const ArticlesByTopic = () => {
   const [articles, setArticles] = useState([]);
-  // const [articleTopic, setArticleTopic] = useState("");
+  const [loading, setLoading] = useState(false);
   const { topicSlug } = useParams();
 
+  console.log(topicSlug);
+
   useEffect(() => {
-    getArticlesByTopic().then((articles) => {
+    setLoading(true);
+    getArticlesByTopic(topicSlug).then((articles) => {
       const articlesByTopic = articles.filter(
         (article) => article.topic === topicSlug
       );
+
       setArticles(articlesByTopic);
+      setLoading(false);
     });
   }, [topicSlug]);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
+      <div>
+        <h3 className="topic-header">
+          This are all articles related to {topicSlug}
+        </h3>
+      </div>
       {articles.map((article) => (
         <div key={article.article_id}>
           <Link to={`/articles/${article.article_id}`}>
